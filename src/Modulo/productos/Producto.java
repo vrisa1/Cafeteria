2d4515e59520cca58780f3ff0bbe734b84971c10
AgostinaFR <afenoyrivas@gmail.com>
2023-06-22 20:47:22 -0300

    Update Producto y subclases
    Cambios en las clases:
    -Producto
    -Comida
    -Bebida
    -BebidaEnvasada
    -Infusion
    Se agregaron y/o modificaron constructores, getters y setters, equals() y toString()

--- src/Modulo/productos/Producto.java
+++ src/Modulo/productos/Producto.java
@@ -1,11 +1,25 @@
 package Modulo.productos;
 
+import java.util.Objects;
+
 public abstract class Producto {
+
+   //ATRIBUTOS--------------------------------------------------------------------------------------------------
     private String nombre;
     private double precio;
     private boolean disponible;
     private int cantidadVendidos;
     private String descripcion;
+
+    //CONSTRUCTORES----------------------------------------------------------------------------------------------
+
+    public Producto() {
+        this.nombre = "";
+        this.precio = 0;
+        this.disponible = false;
+        this.cantidadVendidos = 0;
+        this.descripcion = "";
+    }
 
     public Producto(String nombre, double precio, boolean disponible, int cantidadVendidos, String descripcion) {
         this.nombre = nombre;
@@ -14,6 +28,50 @@ public abstract class Producto {
         this.cantidadVendidos = cantidadVendidos;
         this.descripcion = descripcion;
     }
+
+    //GETTERS Y SETTERS------------------------------------------------------------------------------------------
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public int getCantidadVendidos() {
+        return cantidadVendidos;
+    }
+
+    public void setCantidadVendidos(int cantidadVendidos) {
+        this.cantidadVendidos = cantidadVendidos;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    //TOSTRING------------------------------------------------------------------------------------------------
 
     @Override
     public String toString() {
@@ -25,4 +83,5 @@ public abstract class Producto {
                 ", descripcion='" + descripcion + '\'' +
                 '}';
     }
+
 }
--- src/Modulo/productos/bebidas/BebidaEnvasada.java
+++ src/Modulo/productos/bebidas/BebidaEnvasada.java
@@ -1,13 +1,77 @@
 package Modulo.productos.bebidas;
 
 import Modulo.productos.bebidas.Bebida;
+import Modulo.productos.comidas.Comida;
+
+import java.util.Objects;
 
 public class BebidaEnvasada extends Bebida {
+
+    //ATRIBUTOS-------------------------------------------------------------------------------------------------
 
     private boolean gas;
     private String linea;
 
-    public BebidaEnvasada(String nombre, double precio, int disponibilidad, int cantidadVendidos, String descripcion) {
-        super(nombre, precio, disponibilidad, cantidadVendidos, descripcion);
+    //CONSTRUCTORES---------------------------------------------------------------------------------------------
+
+    public BebidaEnvasada() {
+        super();
+        this.gas = false;
+        this.linea = "";
     }
+
+    public BebidaEnvasada(String nombre, double precio, boolean disponible, int cantidadVendidos, String descripcion,
+                          double tamaño, boolean gas, String linea) {
+        super(nombre, precio, disponible, cantidadVendidos, descripcion, tamaño);
+        this.gas = gas;
+        this.linea = linea;
+    }
+
+    //GETTERS Y SETTERS-----------------------------------------------------------------------------------------
+
+    public boolean isGas() {
+        return gas;
+    }
+
+    public void setGas(boolean gas) {
+        this.gas = gas;
+    }
+
+    public String getLinea() {
+        return linea;
+    }
+
+    public void setLinea(String linea) {
+        this.linea = linea;
+    }
+
+    //EQUALS Y TOSTRING-----------------------------------------------------------------------------------------
+
+    @Override
+    public boolean equals(Object obj) {
+        boolean flag = false;
+        if(obj != null){
+            if(obj == this){
+                flag = true;
+            }
+            else if(obj instanceof BebidaEnvasada){
+                BebidaEnvasada aux = (BebidaEnvasada) obj;
+                if(getNombre().equals(aux.getNombre()) && getTamaño()==aux.getTamaño() &&
+                        getLinea().equals(aux.getLinea()) && isGas()==aux.isGas()){
+                    flag = true;
+                }
+            }
+        }
+        return flag;
+    }
+
+    @Override
+    public String toString() {
+        return "BebidaEnvasada{" +
+                super.toString() +
+                "gas=" + gas +
+                ", linea='" + linea + '\'' +
+                '}';
+    }
+
 }
