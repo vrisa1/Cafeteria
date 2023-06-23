747c78e32abdde1dcd17ef5fd3c45569738f0407
vrisa1 <106835493+vrisa1@users.noreply.github.com>
2023-06-23 10:12:40 -0300

    Revert "Revert "Carrito/Compras""
    This reverts commit 1512bdcb79381fe800c8658c92b37ca1b92ce772.

--- src/Modulo/productos/Producto.java
+++ src/Modulo/productos/Producto.java
@@ -1,11 +1,21 @@
 package Modulo.productos;
 
-public abstract class Producto {
+import Modulo.json.I_Json;
+
+public abstract class Producto implements I_Json {
     private String nombre;
     private double precio;
     private boolean disponible;
     private int cantidadVendidos;
     private String descripcion;
+
+    public Producto() {
+        this.nombre = "";
+        this.precio = 0;
+        this.disponible = true;
+        this.cantidadVendidos = 0;
+        this.descripcion = "";
+    }
 
     public Producto(String nombre, double precio, boolean disponible, int cantidadVendidos, String descripcion) {
         this.nombre = nombre;
@@ -13,5 +23,53 @@ public abstract class Producto {
         this.disponible = disponible;
         this.cantidadVendidos = cantidadVendidos;
         this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public int getCantidadVendidos() {
+        return cantidadVendidos;
+    }
+
+    public void setCantidadVendidos(int cantidadVendidos) {
+        this.cantidadVendidos = cantidadVendidos;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    //temporal; para poder aumentar cuando se venden
+    public void aumentarCantidadVendidos(int cantidad){
+        this.cantidadVendidos+=cantidad;
+    }
+    public void disminuirCantidadVendidos(int cantidad){
+        this.cantidadVendidos-=cantidad;
     }
 }
