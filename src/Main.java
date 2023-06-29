2f124c69b2ef43dcc6a09579820fbac146cb14dc
vrisa1 <106835493+vrisa1@users.noreply.github.com>
2023-06-28 23:40:10 -0300

    Update Main.java
    porque me tira error :/

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-
 import Modulo.archivos.ControladorArchivosObjetos;
+import Modulo.Cafeteria;
+import Modulo.Excepciones.ProductoNoDisponibleException;
+import Modulo.InterfazGrafica.InterfazAdmin;
+import Modulo.InterfazGrafica.Login;
+import Modulo.compras.Compra;
+import Modulo.compras.MetodosDePago;
+import Modulo.json.JsonUtiles;
 import Modulo.productos.Producto;
 import Modulo.productos.bebidas.BebidaEnvasada;
 import Modulo.productos.bebidas.Infusion;
 import Modulo.productos.bebidas.Leche;
 import Modulo.productos.comidas.Comida;
 import Modulo.Cafeteria;
-
-
 import Modulo.usuarios.Usuario;
 import Modulo.genericas.ContenedorMapa;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import java.util.HashSet;
 import java.util.Iterator;
@@ -18,32 +29,45 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Cafeteria cafeteria = new Cafeteria();
+        /*
+        //COMPRAS
+        Cafeteria cafeteria1 = new Cafeteria();
 
+        try{
+            cafeteria1.iniciarCompras();
+        }
+        catch (JSONException e) {
+            System.out.println(e.getMessage());
+        }
 
-
+        Compra compra=new Compra(cafeteria1.getCantidadCompras());
+        Producto infusion= new Infusion("Cafe con leche", 500, true, 0, "blablabla", 2, 150, "caliente", Leche.COCO);
+        Producto envasada= new BebidaEnvasada("Gaseosa", 700, true, 0, "blablabla2", 1, 500, true, "Coca Cola");
         try {
-            cafeteria.CrearUsuario("aa","1234");
-
-            int ingreso = cafeteria.login("aa","1234");
-            System.out.println(ingreso);
-        }catch (Exception e){
-
+            compra.agregarAlCarrito(infusion);
+            compra.agregarAlCarrito(envasada);
+        }
+        catch (ProductoNoDisponibleException e){
+            System.out.println(e.getMessage());
         }
 
+        //hacer elegir al usuario el metodo para finalizar la compra
+        cafeteria1.finalizarCompra(compra, MetodosDePago.CREDITO);
 
+        System.out.println(cafeteria1.mostrarCompras());
 
-
-
-
-        HashSet<Producto> menu = new HashSet<>();
-        menu = ControladorArchivosObjetos.leer("bebidaenvasada.dat");
-        Iterator<Producto> it = menu.iterator();
-        while(it.hasNext()){
-            Producto aux = (BebidaEnvasada) it.next();
-            System.out.println(aux.toString());
+        try{
+            cafeteria1.guardarJsonCompras();
+        }
+        catch (JSONException e){
+            System.out.println(e.getMessage());
         }
 
+         */
+
+
+        //InterfazAdmin aux=new InterfazAdmin();
+        //Login aux1= new Login(new Cafeteria());
 
     }
 }
