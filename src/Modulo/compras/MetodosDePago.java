234d98deedb115b40352348992bff6333b767b52
vrisa1 <106835493+vrisa1@users.noreply.github.com>
2023-06-30 05:52:48 -0300

    Documentacion Compra, MetodosDePago

--- src/Modulo/compras/MetodosDePago.java
+++ src/Modulo/compras/MetodosDePago.java
@@ -1,5 +1,11 @@
 package Modulo.compras;
 
+/**
+ * Clase de tipo enum.
+ * Se utiliza como atributo en la clase Compra.
+ * Inicializado como null en todas las instancias de compra.
+ * Se selecciona la opción deseada al momento de finalizar una compra.
+ */
 public enum MetodosDePago {
     EFECTIVO, DEBITO, CREDITO
 }
